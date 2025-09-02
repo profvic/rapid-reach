a8530721fae57eec2e59945fda3806b243a7909f
Eberechi Victor Nnamdi <victoreberechi0@gmail.com>
2025-09-02 19:51:16 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -109,7 +109,7 @@ mongoose
 // ✅ Serve React frontend
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 
