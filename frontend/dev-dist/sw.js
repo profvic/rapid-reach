7f5f593ed9e89b53d8ba1e7b98e51f054129802c
Eberechi Victor Nnamdi <victoreberechi0@gmail.com>
2026-01-15 09:45:41 +0100

    new commit

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.cqk9bn0ng18"
+    "revision": "0.h10tmesqi7o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
