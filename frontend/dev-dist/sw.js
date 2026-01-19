4e3b5ce49bd24df9e01cd1234edc564eb3b6ce00
Eberechi Victor Nnamdi <victoreberechi0@gmail.com>
2026-01-19 21:24:36 +0100

    Update sw.js

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.h10tmesqi7o"
+    "revision": "0.0umvmsumaeg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
